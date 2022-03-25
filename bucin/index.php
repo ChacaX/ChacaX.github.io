044ac9c5bb9586cdba07522b3e24d287ea9284fb
MITSUHA-XXX <77813696+ChacaX@users.noreply.github.com>
2022-03-26 00:54:44 +0700

    Add files via upload

--- bucin/index.php
+++ bucin/index.php
@@ -1,1 +1,51 @@
 
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>WEBSITE BUCIN</title>
+
+	<style>
+
+		.background {
+			background-image: linear-gradient(to left, red, purple);
+			width: 40%;
+			margin: auto;
+			border-radius: 7px;
+			margin-top: 200px;
+			text-align: center;
+			font-family: cursive;
+			color: white;
+			text-shadow: 0 2px 5px aqua;
+			padding: 10px;
+			font-size: 10px;
+			box-shadow: 4px 7px 0 black;
+   }
+
+		.flex {
+			display: flex;
+	}
+
+	</style>
+</head>
+<body>
+
+
+<div class="background">
+	<br>
+	<h1>FOR YOU BABY</h1>
+	<br>
+		<h1>hallo nona cantik masukin nama mu pada kolom dibawah ini ya. thank you</h1>
+		<br>
+		<form action="page.php" method="post">
+			<label>nama kamu :</label>
+            <input type="text" name="username">
+            <button type="submit" name="submit">submit</button>
+		</form>
+		<br>
+	<br>
+	</div>
+   </div>
+    
+
+</body>
+</html>
---
+++ bucin/page.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BUCIN WEBSITE</title>
+
+	<style>
+
+	.background {
+			background-image: linear-gradient(to left, red, purple);
+			width: 40%;
+			margin: auto;
+			border-radius: 7px;
+			margin-top: 200px;
+			text-align: center;
+			font-family: cursive;
+			color: white;
+			text-shadow: 0 2px 5px aqua;
+			padding: 10px;
+			font-size: 10px;
+			box-shadow: 4px 7px 0 black;
+   }
+
+		.flex {
+			display: flex;
+	}
+
+</style>
+</head>
+<body>
+
+	<div class="background">
+	<br>
+	<center><img style="width: 20px; height: 20px;" src="https//d.top4top.io/p_2275n8f1a0.png"></center>
+	<br>
+    <h1>I LOVE YOU <?= $_POST["username"]; ?> <3</h1>
+	<br>
+	<br>
+	<div class="flex">
+	<a style="margin: auto; padding: 2px; font-family: cursive; text-shadow: 0 0 5px aqua; text-decoration: none; color: white;" href="htpps://github.com/chacax">github</a>
+	<a style="margin: auto; padding: 2px; font-family: cursive; text-shadow: 0 0 5px aqua; text-decoration: none; color: white;" href="htpps://wa.me/6285731261728">whatsapp</a>
+    <a style="margin: auto; padding: 2px; font-family: cursive; text-shadow: 0 0 5px aqua; text-decoration: none; color: white;" href="htpps://github.com/chacax">instagram</a>
+	<a style="margin: auto; padding: 2px; font-family: cursive; text-shadow: 0 0 5px aqua; text-decoration: none; color: white;" href="htpps://github.com/chacax">youtube</a>
+    </div>
+	<br>
+	<br>
+	<center><audio controls src="audio.mp3"></center>
+	<br>
+	<br>
+	</div>
+   </div>s
+
+</body>
+</html>
